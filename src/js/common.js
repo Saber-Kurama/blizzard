9f039c2a2a0e37367ef2a3b8082124d9c7b7b03c
guochao <guochao@weijinsuo.com>
2016-04-20 11:47:29 +0800

    完成菜单的功能

--- src/js/common.js
+++ src/js/common.js
@@ -10,13 +10,37 @@
 var Common = {
   init:function(){
     this.setFastClick();
+    this.setMenu();
     this.setMobileMenu();
   },
   // 设置 FastClick
   setFastClick: function() {
     FastClick.attach(document.body)
   },
-
+  // 设置菜单
+  setMenu: function(){
+    var pureMenu = $('.pure-menu'),
+        pin = $(".menu-main-pin"),
+        cssclass = "menu-hidden",
+        navBtn = $(".nav-btn"),
+        needTop = 44,
+        top = 0;
+    $(window).on("scroll", function(){
+      var scroolltop = $(window).scrollTop();
+      if(scroolltop!== 0){
+        var isShowPin = scroolltop > needTop;
+        isShowPin ? pureMenu.css('position', 'fixed')
+            : pureMenu.css('position', 'absolute'),
+            isShowPin = isShowPin && scroolltop > top,
+            pureMenu.toggleClass(cssclass, isShowPin),
+            top = scroolltop;
+      }
+    });
+    pin.on('mouseenter', function(){
+      pureMenu.removeClass(cssclass);
+    });
+  },
+  // 设置 移动端菜单
   setMobileMenu: function() {
 
     var burgernav = $('.burger-nav'),
